a9f2483ff9c77ecf6574fbe115028d50e2c38479
Wando Wagner Schneider <wandoschneider@gmail.com>
2022-11-13 22:24:30 -0300

    removendo usings

--- src/Play.Catalog.Services/Program.cs
+++ src/Play.Catalog.Services/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Play.Catalog.Services;
 using Play.Catalog.Services.Entities;
 using Play.Common.Configuration;
